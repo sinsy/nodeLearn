996aca76e7ee05ec7490f05cba58e02378d3c3bc
betalun <betalun@qq.com>
2018-11-06 17:37:59 +0800

    'update'

---
+++ express/app.js
@@ -0,0 +1,50 @@
+var fs = require('fs');
+var express = require('express');
+var multer  = require('multer')
+
+var app = express();
+app.use(express.static('public'));
+var createFolder = function(folder){
+  try{
+    fs.accessSync(folder);
+  }catch(e){
+    fs.mkdirSync(folder);
+  }
+};
+
+var uploadFolder = './public/';
+
+createFolder(uploadFolder);
+
+// 通过 filename 属性定制
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, uploadFolder);    // 保存的路径，备注：需要自己创建
+  },
+  filename: function (req, file, cb) {
+    console.log(file)
+
+    // 将保存文件名设置为 字段名 + 时间戳，比如 logo-1478521468943
+    cb(null, file.fieldname + '-' + Date.now()+'.'+file.originalname.split('.')[1]);
+  }
+});
+
+// 通过 storage 选项来对 上传行为 进行定制化
+var upload = multer({ storage: storage })
+
+// 单图上传
+app.post('/upload', upload.single('logo'), function(req, res, next){
+  var file = req.file;
+  console.log(file)
+  res.send({
+    ret_code: '0',
+    url: 'http://127.0.0.1:3000/'+file.filename
+  });
+});
+
+app.get('/form', function(req, res, next){
+  var form = fs.readFileSync('./form.html', {encoding: 'utf8'});
+  res.send(form);
+});
+
+app.listen(3000);
